a28b4b5855e94df3ef77a97b16a506f405f5d4df
Rosga <rozzga@gmail.com>
2019-04-18 14:43:23 +0300

    Added node js bindings for node-gyp

---
+++ binding.gyp
@@ -0,0 +1,112 @@
+{
+    'targets': [
+        {
+            'target_name': 'iohook',
+            'include_dirs': [
+                "<!(node -e \"require('nan')\")",
+                "include",
+                "src"
+            ],
+            'cflags': [
+                '-Wall',
+                '-Wparentheses',
+                '-Winline',
+                '-Wbad-function-cast',
+                '-Wdisabled-optimization',
+                '-fexceptions'
+            ],
+            'cflags_cc': ['-fexceptions'],
+            'cflags!': [ '-fno-exceptions' ],
+            'cflags_cc!': [ '-fno-exceptions' ],
+
+            "msvs_settings": {
+                "VCCLCompilerTool": {
+                'RuntimeLibrary': 2, # shared release
+                'ExceptionHandling': 1,     # /EHsc  doesn't work.
+                'AdditionalOptions': ['/EHsc']
+                }
+            },
+            
+            'conditions': [
+                ['OS == "mac"', {
+                'xcode_settings': {
+                    'GCC_ENABLE_CPP_EXCEPTIONS': 'YES'
+                    },
+                'include_dirs': [
+                    'System/Library/Frameworks/CoreFoundation.Framework/Headers',
+                    'System/Library/Frameworks/Foundation.framework/Headers',
+                    'System/Library/Frameworks/Carbon.Framework/Headers',
+                    'System/Library/Frameworks/ApplicationServices.framework/Headers',
+                    'System/Library/Frameworks/IOKit.framework/Headers',
+                    'System/Library/Frameworks/Cocoa.framework/Headers',
+                    'System/Library/Frameworks/OpenGL.framework/Headers'
+                ],
+                "sources": [
+                    "src/darwin/system_properties.c",
+                    "src/darwin/input_helper.c",
+                    "src/darwin/input_hook.c"
+                ],
+                'link_settings': {
+                    'libraries': [
+                    '-framework Carbon',
+                    '-framework CoreFoundation',
+                    '-framework ApplicationServices',
+                    '-framework Cocoa',
+                    '-framework Foundation',
+                    '-framework OpenGL',
+                    '-framework IOKit'
+                    ]
+                },
+                }],
+                
+                ['OS == "linux"', {
+                'link_settings': {
+                    'libraries': [
+                    '-lpng',
+                    '-lz',
+                    '-lX11',
+                    '-lXtst'
+                    ]
+                },
+                "sources": [
+                    "src/linux/system_properties.c",
+                    "src/linux/input_helper.c",
+                    "src/linux/input_hook.c"
+                ],
+                }],
+
+                ["OS=='win'", {
+                'defines': ['IS_WINDOWS', '_HAS_EXCEPTIONS=1'],
+                'link_settings': {
+                    'libraries': [
+                    'Oleacc',
+                    "UIAutomationCore",
+                    
+                    "comsuppw"
+                    ]
+                },
+                "sources": [
+                    "src/windows/system_properties.c",
+                    "src/windows/input_helper.c",
+                    "src/windows/input_hook.c"
+                ],
+                }]
+            ],
+            'sources': [
+                "src/iohook.cc",
+                "src/logger.c"
+            ]
+        },
+        {
+            "target_name": "release",
+            "type": "none",
+            "dependencies": [ "iohook" ],
+            "copies": [
+                {
+                "files": [ "<(PRODUCT_DIR)/iohook.node" ],
+                "destination": "./lib"
+                }
+            ],
+        },
+    ]
+}
---
+++ src/iohook.cc
@@ -0,0 +1,185 @@
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <node.h>
+#include <nan.h>
+#include <nan_object_wrap.h>
+#include <v8.h>
+#include <vector>
+
+#if defined(USE_X11)
+
+#include "xdisplay.h" 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <locale.h> 
+#include <X11/Xlib.h> 
+#include <X11/Xatom.h> 
+#include <X11/cursorfont.h> 
+#include <X11/Xlib.h> 
+#include <X11/XKBlib.h> 
+#include <X11/Xutil.h> 
+#include "linux/input_helper.h" 
+
+#endif
+
+#include <queue>
+#include <limits>
+#include <inttypes.h>
+#include "uiohook.h"
+
+#if defined(IS_WINDOWS)
+#include <Oleacc.h>
+#include <Winuser.h>
+#endif
+
+#include "iohook.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#if defined(__APPLE__) && defined(__MACH__)
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
+#include <pthread.h>
+#endif
+
+using namespace v8;
+using Callback = Nan::Callback;
+static bool sIsRuning=false; 
+
+static HookProcessWorker* sIOHook = nullptr;
+
+static void dispatch_proc(uiohook_event * const event)
+{
+	if (sIOHook != nullptr && sIOHook->fHookExecution!=nullptr)
+	{
+		sIOHook->fHookExecution->Send(event, sizeof(uiohook_event));
+	}
+}
+
+// static bool logger_proc(unsigned int level, const char *format, ...)
+// {
+// 	return true;
+// }
+
+
+HookProcessWorker::HookProcessWorker(Nan::Callback * callback) :
+Nan::AsyncProgressWorkerBase<uiohook_event>(callback),
+fHookExecution(nullptr)
+{
+	
+}
+
+void HookProcessWorker::Execute(const Nan::AsyncProgressWorkerBase<uiohook_event>::ExecutionProgress& progress)
+{
+	// hook_set_logger_proc(&logger_proc);
+	hook_set_dispatch_proc(&dispatch_proc);
+	fHookExecution = &progress;
+	printf("\n\nhook_run\n\n");
+	hook_run();
+}
+
+void HookProcessWorker::Stop()
+{
+	hook_stop();
+	sIsRuning = false;
+}
+void HookProcessWorker::HandleProgressCallback(const uiohook_event *data, size_t size)
+{
+	HandleScope scope(Isolate::GetCurrent());
+	v8::Local<v8::Object> obj = Nan::New<v8::Object>();
+
+    if (data == NULL || data == nullptr || (data->type == NULL) || data->type < 0) { 
+        return; 
+    }
+
+	Nan::AsyncResource resource("libuiohook-node:addon.HandleProgressCallback");
+	
+	Nan::Set(obj, Nan::New("type").ToLocalChecked(), Nan::New((uint16_t)data->type));
+	Nan::Set(obj, Nan::New("mask").ToLocalChecked(), Nan::New((uint16_t)data->mask));
+	if ((data->type >= EVENT_MOUSE_CLICKED) && (data->type < EVENT_MOUSE_WHEEL))
+	{
+		v8::Local<v8::Object> mouse = Nan::New<v8::Object>();
+		Nan::Set(mouse, Nan::New("button").ToLocalChecked(), Nan::New((int)data->data.mouse.button));
+		Nan::Set(mouse, Nan::New("clicks").ToLocalChecked(), Nan::New((int)data->data.mouse.clicks));
+		Nan::Set(mouse, Nan::New("x").ToLocalChecked(), Nan::New((int)data->data.mouse.x));
+		Nan::Set(mouse, Nan::New("y").ToLocalChecked(), Nan::New((int)data->data.mouse.y));
+
+		Nan::Set(obj, Nan::New("mouse").ToLocalChecked(), mouse);
+		v8::Local<v8::Value> argv[] = { obj };
+		callback->Call(1, argv, &resource);
+	}
+	else if ((data->type >= EVENT_KEY_TYPED) && (data->type <= EVENT_KEY_RELEASED))
+	{
+		v8::Local<v8::Object> keyboard = Nan::New<v8::Object>();
+
+		Nan::Set(keyboard, Nan::New("keychar").ToLocalChecked(), Nan::New((int)data->data.keyboard.keychar));
+		Nan::Set(keyboard, Nan::New("keycode").ToLocalChecked(), Nan::New((int)data->data.keyboard.keycode));
+		Nan::Set(keyboard, Nan::New("rawcode").ToLocalChecked(), Nan::New((int)data->data.keyboard.rawcode));
+		Nan::Set(obj, Nan::New("keyboard").ToLocalChecked(), keyboard);
+		v8::Local<v8::Value> argv[] = { obj };
+		callback->Call(1, argv, &resource);
+	}
+	else if (data->type == EVENT_MOUSE_WHEEL)
+	{
+		v8::Local<v8::Object> wheel = Nan::New<v8::Object>();
+		Nan::Set(wheel, Nan::New("amount").ToLocalChecked(), Nan::New((int)data->data.wheel.amount));
+		Nan::Set(wheel, Nan::New("clicks").ToLocalChecked(), Nan::New((int)data->data.wheel.clicks));
+		Nan::Set(wheel, Nan::New("direction").ToLocalChecked(), Nan::New((int)data->data.wheel.direction));
+		Nan::Set(wheel, Nan::New("rotation").ToLocalChecked(), Nan::New((int)data->data.wheel.rotation));
+		Nan::Set(wheel, Nan::New("type").ToLocalChecked(), Nan::New((int)data->data.wheel.type));
+		Nan::Set(wheel, Nan::New("x").ToLocalChecked(), Nan::New((int)data->data.wheel.x));
+		Nan::Set(wheel, Nan::New("y").ToLocalChecked(), Nan::New((int)data->data.wheel.y));
+
+		Nan::Set(obj, Nan::New("wheel").ToLocalChecked(), wheel);
+		v8::Local<v8::Value> argv[] = { obj };
+
+		callback->Call(1, argv, &resource);
+	}
+	
+}
+
+
+NAN_METHOD(StartHook) {
+
+    //allow one single execution
+    if(sIsRuning==false)
+    {
+        if(info.Length() >0)
+        {
+			if (info[0]->IsFunction())
+			{
+				Callback* callback = new Callback(info[0].As<Function>());
+				sIOHook = new HookProcessWorker(callback);
+				Nan::AsyncQueueWorker(sIOHook);
+				sIsRuning = true;
+			}
+        }
+    }
+}
+
+NAN_METHOD(StopHook) {
+
+	//allow one single execution
+	if ((sIsRuning == true) && (sIOHook !=nullptr))
+	{
+		sIOHook->Stop();
+	}
+}
+
+NAN_MODULE_INIT(InitAll)
+{
+
+    Nan::Set(target, Nan::New<String>("startHook").ToLocalChecked(),
+             Nan::GetFunction(Nan::New<FunctionTemplate>(StartHook)).ToLocalChecked());
+
+    Nan::Set(target, Nan::New<String>("stopHook").ToLocalChecked(),
+             Nan::GetFunction(Nan::New<FunctionTemplate>(StopHook)).ToLocalChecked());
+}
+
+NODE_MODULE(iohookjs, InitAll)
